aa3bf19d4e96923e6a35e2fab042bce6cb67ab1e
mikeoxlonger-ops <mallymal1901@gmail.com>
2025-10-05 21:49:18 -0500

    Create Playlist.jsx

---
+++ src/components/playlist/Playlist.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Playlist() {
+  return (
+    <div className="bg-fl-gray p-4 flex-1">
+      <h2 className="text-white text-lg mb-2">Playlist</h2>
+      <div className="bg-gray-800 h-full rounded p-2">
+        <p className="text-gray-500">Playlist Arrangement View</p>
+      </div>
+    </div>
+  );
+}
+
+export default Playlist;
