0e36f13c34217b03b458ecb5c90136548cf9738d
mikeoxlonger-ops <mallymal1901@gmail.com>
2025-10-05 21:44:10 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'fl-dark': '#1a1a1a',
+        'fl-gray': '#2a2a2a',
+        'fl-accent': '#ff6b35',
+      },
+    },
+  },
+  plugins: [],
+};
